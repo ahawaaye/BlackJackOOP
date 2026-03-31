ed7f210866c0972acc9b5a54f37b277fff1dd634
ahawaaye <ahawa004@gmail.com>
2026-03-31 18:06:12 +0200

    card en classes maken

---
+++ BlackJackOOP/Models/Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackOOP.Models
+{
+    internal class Card
+    {
+        public enum Suit
+        {
+             Hearts,
+            Diamonds,
+            Clubs,
+            Spades
+
+        }
+
+        public enum Rank
+        {
+            Two = 2,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace
+
+
+        }
+        
+        public class card
+        {
+            public Suit suit;
+            public Rank rank;
+
+            public int GetValue()
+            {
+                 
+            }
+
+        }
+    }
+}
