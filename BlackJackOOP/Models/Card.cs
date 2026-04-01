9301462f9e09afdbb52cd7a20ae1a08a3ddfbe9a
ahawaaye <ahawa004@gmail.com>
2026-04-01 11:42:10 +0200

    nieuwe branch  gemaakt voor de eerste user story en mijn deck en card systeem ingezet

--- BlackJackOOP/Models/Card.cs
+++ BlackJackOOP/Models/Card.cs
@@ -41,7 +41,16 @@ namespace BlackJackOOP.Models
 
             public int GetValue()
             {
-                 
+                //numbers 2-10 
+                if((int)rank <= 10) // the value of the cards (2-10) is the same as their rank
+                {
+                    return (int)rank;
+                }
+                // the faces of the cards (they all return the value of 10)
+                if (rank == Rank.Jack || rank == Rank.Queen || rank == Rank.King)
+                    return 10; 
+
+                // ace can be worth either 1 or 11 but for defult we will return 11
             }
 
         }
